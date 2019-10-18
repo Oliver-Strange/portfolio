b1bcc4a297822192bd05ee63b0ebba28d8219d0e
Oliver-Strange <preston.burton@gmail.com>
2019-10-17 20:46:00 -0700

    adds to styling

--- portfolio/src/components/landing/landingHeader.jsx
+++ portfolio/src/components/landing/landingHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn, faAngellist } from "@fortawesome/free-brands-svg-icons";
-import { faFile, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 function LandingHeader() {
   return (
